25390028ddc7393f9a4faccaa8c9c83694ed0a3b
trentbutikofer <trent.butikofer@hotmail.com>
2019-06-12 23:26:59 -0600

    flex3

--- assignments/lesson-7/js/windchill.js
+++ assignments/lesson-7/js/windchill.js
@@ -1,15 +1,10 @@
-function windchill() {
 
-    var temp = parseInt(document.getElementById("temp").value);
-    var windspeed = parseInt(document.getElementById("windspeed").value);
+    var temp = parseInt(document.getElementById("temp").innerHTML);
+    var windspeed = parseInt(document.getElementById("windspeed").innerHTML);
 
     var windchill = Math.round(35.74 + (0.6215 * temp) -
                               (35.75 * Math.pow(windspeed, 0.16)) + 
                               (0.4275 * temp * Math.pow(windspeed, 0.16)));
 
-    if (isNaN(windchill)) {
-        document.getElementById("windchill").innerHTML = "Wind Chill:";
-    } else {
-        document.getElementById("windchill").innerHTML = "Wind Chill: " + windchill;
-    }   
-}
+    document.getElementById("windchill").innerHTML += windchill;
+
